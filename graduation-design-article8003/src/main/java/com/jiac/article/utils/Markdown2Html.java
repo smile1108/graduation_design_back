0c7191867be7ee99700bcc977c5fbc6fb0fa8d7f
Jia Chang <smile001108@163.com>
2022-03-03 12:35:51 +0800

    [back] 1. complete Markdown2Html and Html2Text util and test successfully

---
+++ graduation-design-article8003/src/main/java/com/jiac/article/utils/Markdown2Html.java
@@ -0,0 +1,23 @@
+package com.jiac.article.utils;
+
+import com.vladsch.flexmark.html.HtmlRenderer;
+import com.vladsch.flexmark.parser.Parser;
+import com.vladsch.flexmark.util.ast.Node;
+import com.vladsch.flexmark.util.data.MutableDataSet;
+
+/**
+ * FileName: Markdown2Html
+ * 引用自博客 https://blog.csdn.net/Piconjo/article/details/105476021
+ */
+public class Markdown2Html {
+    public static String convert(String md) {
+        MutableDataSet options = new MutableDataSet();
+
+        Parser parser = Parser.builder(options).build();
+        HtmlRenderer renderer = HtmlRenderer.builder(options).build();
+
+        Node document = parser.parse(md);
+        String html = renderer.render(document);
+        return html;
+    }
+}
--- graduation-design-article8003/src/test/java/com/jiac/article/GraduationDesignArticle8003ApplicationTests.java
+++ graduation-design-article8003/src/test/java/com/jiac/article/GraduationDesignArticle8003ApplicationTests.java
@@ -1,5 +1,7 @@
 package com.jiac.article;
 
+import com.jiac.article.utils.Html2Text;
+import com.jiac.article.utils.Markdown2Html;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -10,4 +12,36 @@ class GraduationDesignArticle8003ApplicationTests {
     void contextLoads() {
     }
 
+
+    @Test
+    void testMarkdown2Html() {
+        String markdownStr = "## Redis持久化\n" +
+                "\n" +
+                "Redis是内存数据库，如果不将内存中的数据库状态保存到磁盘，那么一旦服务器进程退出，服务器中的数据库状态就会消失。所以Redis提供了持久化功能！\n" +
+                "\n" +
+                "### RDB(Redis DataBase)\n" +
+                "\n" +
+                "> 什么是RDB\n" +
+                "\n" +
+                "![RDB工作方式](./1.png)\n" +
+                "\n" +
+                "在指定的时间间隔内将内存中的数据集快照写入磁盘，也就是行话讲的Snapshot快照，它恢复时是将快照文件直接读到内存里";
+        String html = Markdown2Html.convert(markdownStr);
+        System.out.println(html);
+    }
+
+    @Test
+    void testHtml2Text() {
+        String htmlStr = "<h2>Redis持久化</h2>\n" +
+                "<p>Redis是内存数据库，如果不将内存中的数据库状态保存到磁盘，那么一旦服务器进程退出，服务器中的数据库状态就会消失。所以Redis提供了持久化功能！</p>\n" +
+                "<h3>RDB(Redis DataBase)</h3>\n" +
+                "<blockquote>\n" +
+                "<p>什么是RDB</p>\n" +
+                "</blockquote>\n" +
+                "<p><img src=\"./1.png\" alt=\"RDB工作方式\" /></p>\n" +
+                "<p>在指定的时间间隔内将内存中的数据集快照写入磁盘，也就是行话讲的Snapshot快照，它恢复时是将快照文件直接读到内存里</p>";
+
+        String textStr = Html2Text.convert(htmlStr);
+        System.out.println(textStr);
+    }
 }
