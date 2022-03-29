50bbe9dac6453a02e8764f41d3e7e8717bb1664d
Jia Chang <smile001108@163.com>
2022-03-29 11:37:31 +0800

    [back] chat module config eureka client

--- graduation-design-chat8005/src/main/java/com/jiac/chat/GraduationDesignChat8005Application.java
+++ graduation-design-chat8005/src/main/java/com/jiac/chat/GraduationDesignChat8005Application.java
@@ -2,8 +2,14 @@ package com.jiac.chat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EntityScan("com.jiac.common.entity")
+@EnableFeignClients
 public class GraduationDesignChat8005Application {
 
     public static void main(String[] args) {
